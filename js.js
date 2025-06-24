74b051e5d534ded7d3b7fa439d98cde7dc48fe35
nwp <160255642+Nameyaimak@users.noreply.github.com>
2025-06-24 18:30:49 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,22 +1,35 @@
 function showModal(name, messageHTML) {
-      document.getElementById('friendName').textContent = name;
-      document.getElementById('message').innerHTML = messageHTML;
-      document.getElementById('modal').style.display = 'flex';
-    }
+  document.getElementById("friendName").textContent = name;
+  document.getElementById("message").innerHTML = messageHTML;
+  document.getElementById("modal").style.display = "flex";
+}
 
-    function closeModal() {
-      document.getElementById('modal').style.display = 'none';
-      document.getElementById('message').innerHTML = '';
-    }
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("message").innerHTML = "";
+}
 
-    window.onclick = function(e) {
-      if (e.target.id === 'modal') {
-        closeModal();
-      }
-    }
-    function scrollToTop() {
+window.onclick = function (e) {
+  if (e.target.id === "modal") {
+    closeModal();
+  }
+};
+function scrollToTop() {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: "smooth",
+  });
+}
+function filterFriends() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const friendButtons = document.querySelectorAll(".friend-btn");
+
+  friendButtons.forEach((btn) => {
+    const name = btn.innerText.toLowerCase();
+    if (name.includes(input)) {
+      btn.style.display = "flex";
+    } else {
+      btn.style.display = "none";
+    }
   });
 }
